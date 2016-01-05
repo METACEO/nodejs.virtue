dcd84f5764b0c53e42f25397ed447a3376c980f0
METACEO <metaceo@users.noreply.github.com>
2016-01-05 12:48:27 -0600

    Initial creation.

---
+++ cli.js
@@ -0,0 +1,329 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const VIRTUE  = require("virtue");
+const OS      = require("os");
+const PACKAGE = require("./package.json");
+const PRO     = process;
+const report  = {error:false};
+let arg  = 0;
+let args = PRO.argv.slice(2);
+let json = false;
+let src  = [];
+let srcs = [];
+
+/* If the user wants a JSON
+// response, for use in another
+// application or whatever, then
+// raise the flag.
+*/
+if(args[0] === "--json"){
+	
+	json = true;
+	
+	arg++;
+	
+}
+
+/* Print out the available help
+// if requested and finish.
+*/
+if(args[arg] === "--help"){
+	
+	Message("HELP");
+	
+	Finish();
+	
+}
+
+/* If there are no longer any
+// arguments then tell the user.
+*/
+if(args.length === arg) EndWith("NO-ARGUMENTS");
+
+/* If the available hashes are
+// requested then return them
+// back to the user and finish.
+*/
+if(args[arg] === "--hashes"){
+	
+	Message("HASHES",require("crypto").getHashes());
+	
+	Finish();
+	
+}
+
+Message("STARTING");
+
+/* Start looking through the
+// first of the arguments for
+// any valid hashes to use as
+// the defaults. Once no more
+// are found, then apply and
+// continue to the resources.
+*/
+for(; arg < args.length; arg++){
+	
+	if(args[arg].substr(0,2) !== "--") break;
+	
+	src.push(args[arg].substr(2));
+	
+	/* We keep applying a new
+	// array to Virtue until
+	// we get caught, if false
+	// then we know the latest
+	// src is invalid.
+	*/
+	if(VIRTUE.defaultHashes(src)) continue;
+	
+	EndWith("INVALID-HASH",src.pop());
+	
+}
+
+/* If there are no more arguments
+// supplied then there is nothing
+// more to process.
+*/
+if(args.length === arg) EndWith("NO-RESOURCES");
+
+/* Start going down the line of
+// arguments to collect resources.
+// Any identified hash is applied
+// directly to the resource before
+// it. We know args[arg] going into
+// the while is a resource, so the
+// if statement will only be caught
+// after srcs is prepared and ready.
+*/
+while(args[arg]){
+	
+	if(args[arg].substr(0,2) === "--"){
+		
+		srcs[srcs.length-1][0].push(args[arg].substr(2));
+		
+	}
+	else{
+		
+		srcs.push([[],args[arg]]);
+		
+	}
+	
+	arg++;
+	
+}
+
+/* After we have built the collection
+// of resources, we simply the build
+// to something Virtue can use.
+*/
+srcs = srcs.map((src) => (src[0].length === 0) ? src[1] : src );
+
+/* Finally call Virtue with the
+// resources and their hashes we've
+// collected and respond to the
+// user appropriately.
+*/
+VIRTUE.apply(null,srcs)
+.then(
+	function(results){
+		
+		Message("RESULTS",results);
+		
+		Finish();
+		
+	},
+	function(error){
+		
+		switch(error.code){
+			
+			case "INVALID-RESOURCE": EndWith(error.code,JSON.stringify(srcs[error.arg-1]));
+			
+			case "INVALIDHASH": EndWith(error.code,error.hash);
+			
+			default: EndWith("UNKNOWN",error);
+			
+		}
+		
+	}
+);
+
+/* Output the message, and its data.
+*/
+function Message(message,data){
+	
+	switch(message){
+		
+		case "HELP":{
+			
+			report.platform = PRO.platform;
+			report.release  = OS.release();
+			report.arch     = OS.arch();
+			report.openssl  = PRO.versions.openssl;
+			report.node     = PRO.versions.node;
+			report.version  = PACKAGE.version;
+			report.message  = "Submit arguments below in order from top-down:\n" +
+			"\n" +
+			"$ virtue\n" +
+			"  [--json] Will print output in JSON only.\n" +
+			"  [--help] Display this.\n" +
+			"  [--hashes] List out the available hashes from Node's crypto module.\n" +
+			"  [[--hash] ..] Override the default hash with these.\n" +
+			"  <resource [[--hash] ..]> Adds a resource using any provided hashes.\n" +
+			"\n" +
+			"Find more information at " + PACKAGE.homepage;
+			
+			if(!(json)){
+				
+				console.log(" ");
+				console.log("Platform: " + report.platform);
+				console.log("Release: " + report.release);
+				console.log("Arch: " + report.arch);
+				console.log("OpenSSL: " + report.openssl);
+				console.log("Node: " + report.node);
+				console.log("Virtue: " + report.version);
+				console.log(report.message);
+				
+			}
+			
+			break;
+			
+		}
+		
+		case "STARTING":{
+			
+			if(!(json)) console.log('Starting...');
+			
+			break;
+			
+		}
+		
+		case "HASHES":{
+			
+			report.hashes = data;
+			
+			if(!(json)) console.log(data);
+			
+			break;
+			
+		}
+		
+		case "RESULTS":{
+			
+			report.result = data;
+			
+			if(!(json)){
+				
+				console.log("Printing digests...\n");
+				
+				for(let resource of data){
+					
+					console.log("  " + resource.src);
+					
+					for(let digest in resource.digests){
+						
+						console.log("    " + digest + " === " + resource.digests[digest]);
+						
+					}
+					
+					console.log("  ");
+					
+				}
+				
+			}
+			
+			break;
+			
+		}
+		
+		case "DONE":{
+			
+			if(json) break;
+			
+			console.log('Done!');
+			
+		}
+		
+	}
+	
+}
+
+/* Output the appropriate error
+// response with the any available
+// information, close and finish.
+*/
+function EndWith(error,data){
+	
+	let message;
+	
+	switch(error){
+		
+		case "NO-ARGUMENTS":{
+			
+			message = "No arguments were provided.";
+			
+			break;
+			
+		}
+		
+		case "NO-RESOURCES":{
+			
+			message = "No resources were provided.";
+			
+			break;
+			
+		}
+		
+		case "INVALID-RESOURCE":{
+			
+			message = "An invalid resource was provided => " + data;
+			
+			break;
+			
+		}
+		
+		case "INVALID-HASH":{
+			
+			message = "An invalid hash was provided => " + data;
+			
+			if(json) report.hash = data;
+			
+			break;
+			
+		}
+		
+		case "UNKNOWN":{
+			
+			message = data;
+			
+			if(json) break;
+			
+			console.log(message);
+			
+			message = "The above is all what we know!";
+			
+		}
+		
+	}
+	
+	report.error   = error;
+	report.message = message;
+	
+	if(!(json)) console.log("Error'd:",message,"\nUse virtue --help for additional information.");
+	
+	Finish();
+	
+}
+
+/* Close and finish up the process.
+*/
+function Finish(){
+	
+	Message("DONE");
+	
+	if(json) console.log(JSON.stringify(report));
+	
+	PRO.exit(0);
+	
+}
+
---
+++ index.js
@@ -0,0 +1,337 @@
+'use strict';
+
+const CRYPTO = require("crypto");
+const FS     = require("fs");
+const HTTP   = require("http");
+const HTTPS  = require("https");
+const HASHES = CRYPTO.getHashes();
+let _default = ["sha256"];
+
+module.exports = exports = Virtue;
+
+/* Accepting lookups from here.
+*/
+function Virtue(){
+	
+	const srcs = [];
+	
+	/* Start cycling through the provided sub-
+	// resources.
+	*/
+	for(let arg = 0; arg < arguments.length; arg++){
+		
+		let src = arguments[arg];
+		
+		/* Tests for just a resource and if so
+		// then use the `_default` hashes.
+		//   example:
+		//   "resource.js",
+		//   ...
+		*/
+		if(ValidString(src)) src = [_default,src];
+		
+		/* Otherwise, test for a correct list,
+		// something that includes either a
+		// single string or an array for hashes
+		// and a string resource, if not
+		// then return an error.
+		//   example:
+		//   ["sha256","resource.js"],
+		//   [["sha256","sha384"],"resource.js"],
+		//   ...
+		*/
+		if(
+			(!(Array.isArray(src)))
+			||
+			(src.length !== 2)
+			||
+			(typeof src !== "string")
+			&&
+			(!(Array.isArray(src)))
+			||
+			(!(ValidString(src[1])))
+		){
+			
+			return ErrorArgument(arg);
+			
+		}
+		
+		/* Do we have a single string for
+		// a hash?.. if so then turn it
+		// into a one-item array.
+		//   example:
+		//   ["sha256","subresource.js"],
+		*/
+		src[0] = (Array.isArray(src[0])) ? src[0] : [src[0]];
+		
+		/* We now have an array of hashes...
+		// if our array includes an invalid
+		// hash, then return an error to our
+		// user that'll specify it.
+		*/
+		if(src[0].some(InvalidHash)) return ErrorHash(src[0],arg);
+		
+		/* Push the resource lookup towards the
+		// othes and continue to the next.
+		*/
+		srcs.push(new Promise(Lookup.bind(null,src[0],src[1])));
+		
+	}
+	
+	/* Return the Promise to the user
+	// and allow them to do what they
+	// want with its results.
+	*/
+	return Promise.all(srcs);
+	
+}
+
+/* Provide the user the ability to
+// specify default hashes to digest
+// and return.
+*/
+Virtue.defaultHashes = function DefaultHashes(
+	hashes
+){
+	
+	/* The user can specify multiple
+	// hashes, with a string or an
+	// array, but turn strings into
+	// single-item arrays.
+	//   example:
+	//   .defaultHashes("sha256");
+	//   .defaultHashes(["sha256"]);
+	//   .defaultHashes(["sha256","sha512"]);
+	*/
+	hashes = (Array.isArray(hashes)) ? hashes : [hashes];
+	
+	/* If any erroneous hashes were
+	// provided, or none were provided
+	// at all, then return without
+	// modifying the default...
+	*/
+	if(hashes.length === 0 || hashes.some(InvalidHash)) return false;
+	
+	/* ...otherwise overwrite the
+	// default hashes and return true.
+	*/
+	_default = hashes;
+	
+	return true;
+	
+};
+
+/* Prepare the hashes and determine
+// the resource's scope, local or
+// remote, HTTP or HTTPS.
+*/
+function Lookup(
+	hashes,
+	src,
+	resolve,
+	reject
+){
+	
+	const digests = {};
+	let resource;
+	
+	/* Copy the arguments, don't
+	// reference the `arguments`
+	// object.
+	*/
+	hashes = hashes.slice(0);
+	src    = src.substring(0);
+	
+	/* Create the hash classes
+	// according to their value.
+	*/
+	for(let hash in hashes) digests[hashes[hash]] = CRYPTO.createHash(hashes[hash]);
+	
+	/* Determine the type of
+	// lookup, remote or local.
+	*/
+	if(src.substring(0,7) === "http://"){
+		
+		resource = HTTP.get(src,Hooks.bind(null,digests,src,resolve));
+		
+	}
+	else if(src.substring(0,8) === "https://"){
+		
+		resource = HTTPS.get(src,Hooks.bind(null,digests,src,resolve));
+		
+	}
+	else{
+		
+		resource = Hooks.call(null,digests,src,resolve,FS.ReadStream(src));
+		
+	}
+	
+	/* Catch any errors the lookup
+	// may create.
+	*/
+	resource.on("error",reject);
+	
+}
+
+/* Attach the handlers and the
+// information necessary to the
+// connection, local or remote.
+*/
+function Hooks(
+	digests,
+	src,
+	resolve,
+	connection
+){
+	
+	/* This applies to both remote
+	// connections, HTTP and HTTPS,
+	// and to local read streams.
+	// Return them all.
+	*/
+	return connection.setEncoding("utf8")
+	.on("data",Data.bind(null,digests))
+	.on("end",End.bind(null,digests,src,resolve));
+	
+}
+
+/* As data comes in, add it to
+// each of the hash classes.
+*/
+function Data(
+	digests,
+	data
+){
+	
+	for(let digest in digests) digests[digest].update(data);
+	
+}
+
+/* When all the data is collected,
+// replace the hash classes with
+// their end result.
+*/
+function End(
+	digests,
+	src,
+	resolve
+){
+	
+	/* Start the digestion.
+	*/
+	// Developers!.. Developers!.. Developers!..
+	for(let digest in digests) digests[digest] = digests[digest].digest("base64");
+	// Developers!
+	
+	/* Return the finished result.
+	*/
+	resolve({src,digests});
+	
+}
+
+/* This packages up an error for the
+// user with a message and a code,
+// and if any other information.
+*/
+function ReturnError(
+	message,
+	code,
+	info
+){
+	
+	const err = new Error(message);
+	err.code  = code;
+	
+	/* Append additional information
+	// to the error.
+	*/
+	for(let z in info) err[z] = info[z];
+	
+	return err;
+	
+}
+
+/* This replaces a few calls alerting
+// the user that some provided argument
+// to lookup is invalid.
+*/
+function ErrorArgument(
+	arg
+){
+	
+	const info = {};
+	
+	/* If we get an `arg` number, then
+	// add one to represent the user's
+	// actual input.
+	*/
+	if(typeof arg === "number") info.arg = arg++;
+	
+	return Promise.reject(ReturnError("Invalid resource provided.","INVALID-RESOURCE",info));
+	
+}
+
+/* This alerts the user that some hash
+// is not included in their `CRYPTO`,
+// whether by misspelling of lack of
+// `CRYPTO` features.
+*/
+function ErrorHash(
+	hashes,
+	arg
+){
+	
+	const info = {};
+	
+	/* If we get an `arg` number, then
+	// add one to represent the user's
+	// actual input.
+	*/
+	if(typeof arg === "number") info.arg = arg + 1;
+	
+	/* Find and return the first
+	// erroneous hash.
+	*/
+	if(Array.isArray(hashes)) info.hash = hashes.find(InvalidHash);
+	
+	/* Catch the Promise and
+	// return all available
+	// information.
+	*/
+	return Promise.reject(ReturnError("This hash is unavailable.","INVALID-HASH",info));
+	
+}
+
+/* Check if something is a string
+// type and if it has anything.
+*/
+function ValidString(
+	something
+){
+	
+	return typeof something === "string" && something.length > 0;
+	
+}
+
+/* First test if it's even a
+// string, then if it's included
+// in the collected `HASHES`.
+*/
+function ValidHash(
+	something
+){
+	
+	return ValidString(something) && HASHES.indexOf(something) >= 0;
+	
+}
+
+/* Turn `ValidHash` around.
+*/
+function InvalidHash(
+	something
+){
+	
+	return !(ValidHash(something));
+	
+}
+
